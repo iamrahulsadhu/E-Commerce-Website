ad246f114afe303f8eecc060a45ddff2fa279dea
Rahul Sadhu <rahuls96411@gmail.com>
2024-03-07 23:11:22 +0530

    first commit

--- server/Database/db.js
+++ server/Database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const Connection = async (username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@ecommerce-web.vidkbsa.mongodb.net/?retryWrites=true&w=majority`;
+    const URL = `mongodb+srv://${username}:${password}@ecommerce-web.vidkbsa.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce-web`;
     try{
         await mongoose.connect(URL, {useUnifiedTopology: true, useNewUrlParser: true});
         console.log('Database connected successfully');
